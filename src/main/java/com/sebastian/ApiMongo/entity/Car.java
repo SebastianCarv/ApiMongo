39177686581a7886aeeae237898710a088677b54
Sebastian Alejandro Carvajal Vasquez <sebastianalejandrocarvajalvasquez@MacBook-Pro-de-Sebastian.local>
2021-02-21 14:21:56 -0600

    aca la @Data sin esta solo trae los id pero no carga la info de cada documento

--- src/main/java/com/sebastian/ApiMongo/entity/Car.java
+++ src/main/java/com/sebastian/ApiMongo/entity/Car.java
@@ -1,22 +1,25 @@
 package com.sebastian.ApiMongo.entity;
 
 import com.mongodb.lang.NonNull;
+import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.io.Serializable;
 
 @Document(collection = "cars")
-
+@Data
 public class Car implements Serializable {
     @Id
     @NonNull
     private String _id;
-    @NonNull
+
     private String model;
-    @NonNull
+
     private String brand;
-    @NonNull
+
     private Integer year;
+
+    private String color;
 
 }
